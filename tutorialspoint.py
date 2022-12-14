823174d8120bd5f31087a9ffa93a08a610b4f7c7
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-12-14 10:08:57 +0300

    some updates

--- tutorialspoint.py
+++ tutorialspoint.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 
 
-base_url="https://www.tutorialspoint.com"
+base_url="https://www.tutorialspoint.com/python"
 
 # ## scrap data from sourcepedia
 
-# source=requests.get(base_url+"/python/index.htm",
+# source=requests.get(base_url+"/index.htm",
 #     # proxies={'http': '222.255.169.74:8080'},
 #     timeout=5)
 # soup=BeautifulSoup(source.text,'html')
@@ -22,14 +22,14 @@ base_url="https://www.tutorialspoint.com"
 #         print('name',a.text,"tutor url",a['href'])
 #     print('single sec end')
 
-endpoint = "/python/python_variables.htm"
+endpoint = base_url+"/index.htm"
 
 
 def check_classes_ids(tag):
-    return (tag.name == 'div' and (tag.get('class') in [['clear'], ["mui-container-fluid", "button-borders"], ['clearer']] or tag.get('id') in ['google-top-ads','load', "google-bottom-ads",'bottom_navigation'])) or tag.name == 'style'
+    return (tag.name == 'div' and (tag.get('class') in [['cover'], ['clear'], ['center-aligned', 'tutorial-menu'], ["mui-container-fluid", "button-borders"], ['clearer']] or tag.get('id') in ['google-top-ads','load', "google-bottom-ads",'bottom_navigation'])) or tag.name == 'style'
 
 def scrap_lesson(endpoint):
-    source=requests.get(base_url+endpoint,
+    source=requests.get(endpoint,
         # proxies={'http': '222.255.169.74:8080'},
         timeout=15)
     soup=BeautifulSoup(source.text,'html')
