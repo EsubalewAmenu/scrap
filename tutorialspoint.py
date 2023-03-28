bc85517efd6d07eda6ce8b01a11989caab1181fc
Esubalew <esubalew.a2009@gmail.com>
2023-03-28 15:59:41 +0300

    some coureses added

--- tutorialspoint.py
+++ tutorialspoint.py
@@ -44,33 +44,6 @@ def scrap_content(endpoint):
     return ''.join([str(x) for x in contents.children])
     # return [content for content in contents.children]
 
-def start_scraping_lessons(): 
-    sections = {
-'Tutorial': [
-{'name': 'Home', 'url': '/bitcoin/index.htm' },
-{'name': 'Introduction', 'url': '/bitcoin/bitcoin_introduction.htm' },
-{'name': 'Environmental Setup', 'url': '/bitcoin/bitcoin_environmental_setup.htm' },
-{'name': 'Blockchain Technology', 'url': '/bitcoin/bitcoin_blockchain_technology.htm' },
-{'name': 'Cryptocurrencies', 'url': '/bitcoin/bitcoin_cryptocurrencies.htm' },
-{'name': 'Features', 'url': '/bitcoin/bitcoin_features.htm' },
-{'name': 'How do they work', 'url': '/bitcoin/bitcoin_how_do_they_work.htm' },
-{'name': 'Wallet', 'url': '/bitcoin/bitcoin_wallet.htm' },
-{'name': 'Mining', 'url': '/bitcoin/bitcoin_mining.htm' },
-{'name': 'Exchanges', 'url': '/bitcoin/bitcoin_exchanges.htm' },
-{'name': 'Trading', 'url': '/bitcoin/bitcoin_trading.htm' },
-{'name': 'Glossary', 'url': '/bitcoin/bitcoin_glossary.htm' },
-{'name': 'Applications', 'url': '/bitcoin/bitcoin_applications.htm' },
-{'name': 'Future', 'url': '/bitcoin/bitcoin_future.htm' },
-{'name': 'Quick Guide', 'url': '/bitcoin/bitcoin_quick_guide.htm' },
-{'name': 'Useful Resources', 'url': '/bitcoin/bitcoin_useful_resources.htm' },
-{'name': 'Discussion', 'url': '/bitcoin/bitcoin_discussion.htm' },
-],
-}
-    scrap_lessons(sections)
-
-
-
-
 def add_scrap_content_to_db(tutorial, section, lesson, content):
     data = {'tutorial': tutorial,'section': section,'lesson': lesson,'content': content}
     # return requests.post("https://dashencon.com/test/wp-json/ds_lc/v1/lesson/add",
@@ -101,10 +74,90 @@ def scrap_lessons(sections):
             time.sleep(10)
 
 
+def start_scraping_lessons(): 
+    sections = {
+'Tutorial': [
+{'name': 'Home', 'url': '/css/index.htm' },
+{'name': 'Introduction', 'url': '/css/what_is_css.htm' },
+{'name': 'Syntax', 'url': '/css/css_syntax.htm' },
+{'name': 'Inclusion', 'url': '/css/css_inclusion.htm' },
+{'name': 'Measurement Units', 'url': '/css/css_measurement_units.htm' },
+{'name': 'Colors', 'url': '/css/css_colors.htm' },
+{'name': 'Backgrounds', 'url': '/css/css_backgrounds.htm' },
+{'name': 'Fonts', 'url': '/css/css_fonts.htm' },
+{'name': 'Text', 'url': '/css/css_text.htm' },
+{'name': 'Images', 'url': '/css/css_images.htm' },
+{'name': 'Links', 'url': '/css/css_links.htm' },
+{'name': 'Tables', 'url': '/css/css_tables.htm' },
+{'name': 'Borders', 'url': '/css/css_borders.htm' },
+{'name': 'Margins', 'url': '/css/css_margins.htm' },
+{'name': 'Lists', 'url': '/css/css_lists.htm' },
+{'name': 'Padding', 'url': '/css/css_padding.htm' },
+{'name': 'Cursors', 'url': '/css/css_cursors.htm' },
+{'name': 'Outlines', 'url': '/css/css_outlines.htm' },
+{'name': 'Dimension', 'url': '/css/css_dimension.htm' },
+{'name': 'Scrollbars', 'url': '/css/css_scrollbars.htm' },
+],
+'Advanced': [
+{'name': 'Visibility', 'url': '/css/css_visibility.htm' },
+{'name': 'Positioning', 'url': '/css/css_positioning.htm' },
+{'name': 'Layers', 'url': '/css/css_layers.htm' },
+{'name': 'Pseudo Classes', 'url': '/css/css_pseudo_classes.htm' },
+{'name': 'Pseudo Elements', 'url': '/css/css_pseudo_elements.htm' },
+{'name': '@ Rules', 'url': '/css/css_at_rules.htm' },
+{'name': 'Text Effects', 'url': '/css/css_text_effects.htm' },
+{'name': 'Media Types', 'url': '/css/css_media_types.htm' },
+{'name': 'Paged Media', 'url': '/css/css_paged_media.htm' },
+{'name': 'Aural Media', 'url': '/css/css_aural_media.htm' },
+{'name': 'Printing', 'url': '/css/css_printing.htm' },
+{'name': 'Layouts', 'url': '/css/css_layouts.htm' },
+{'name': 'Validations', 'url': '/css/css_validations.htm' },
+],
+'CSS3 Tutorial': [
+{'name': 'CSS3 - Tutorial', 'url': '/css/css3_tutorial.htm' },
+{'name': 'CSS3 - Rounded Corner', 'url': '/css/css3_rounded_corner.htm' },
+{'name': 'CSS3 - Border Images', 'url': '/css/css3_boarder_image.htm' },
+{'name': 'CSS3 - Multi Background', 'url': '/css/css3_multi_background.htm' },
+{'name': 'CSS3 - Color', 'url': '/css/css3_color.htm' },
+{'name': 'CSS3 - Gradients', 'url': '/css/css3_gradients.htm' },
+{'name': 'CSS3 - Shadow', 'url': '/css/css3_shadow.htm' },
+{'name': 'CSS3 - Text', 'url': '/css/css3_text.htm' },
+{'name': 'CSS3 - Web font', 'url': '/css/css3_web_font.htm' },
+{'name': 'CSS3 - 2d transform', 'url': '/css/css3_2d_transform.htm' },
+{'name': 'CSS3 - 3d transform', 'url': '/css/css3_3d_transform.htm' },
+{'name': 'CSS3 - Animation', 'url': '/css/css3_animation.htm' },
+{'name': 'CSS3 - Multi columns', 'url': '/css/css3_multi_columns.htm' },
+{'name': 'CSS3 - User Interface', 'url': '/css/css3_userinterface.htm' },
+{'name': 'CSS3 - Box Sizing', 'url': '/css/css3_box_sizing.htm' },
+],
+'Responsive': [
+{'name': 'Responsive Web Design', 'url': '/css/css_responsive.htm' },
+],
+'References': [
+{'name': 'Questions and Answers', 'url': '/css/css_questions_answers.htm' },
+{'name': 'Quick Guide', 'url': '/css/css_quick_guide.htm' },
+{'name': 'References', 'url': '/css/css_references.htm' },
+{'name': 'Color References', 'url': '/css/css_color_references.htm' },
+{'name': 'Web browser References', 'url': '/css/css_web_browser.htm' },
+{'name': 'Web safe fonts', 'url': '/css/css_web_fonts.htm' },
+{'name': 'Units', 'url': '/css/css_units.htm' },
+{'name': 'Animation', 'url': '/css/css_animation.htm' },
+],
+'tools': [
+{'name': 'PX to EM converter', 'url': '/css/css_converter.htm' },
+{'name': 'Color Chooser & Animation', 'url': '/css/css_color_picker.htm' },
+],
+'Resources': [
+{'name': 'Useful Resources', 'url': '/css/css_useful_resources.htm' },
+{'name': 'Discussion', 'url': '/css/css_discussion.htm' },
+],
+}
+    scrap_lessons(sections)
+
 
 # python, java
-course_name = "Bitcoin"
-tutorial = "bitcoin"
+course_name = "CSS"
+tutorial = "css"
 base_url="https://www.tutorialspoint.com"
 
 
