271937332ff413298a0f724b8d6faec8e3b5e31f
Esubalew <esubalew.a2009@gmail.com>
2023-07-26 09:50:31 +0300

    scraping question on progress

--- prepware/questions.py
+++ prepware/questions.py
@@ -25,29 +25,6 @@ class Questions:
 
 def prepare_data(question_list, subject_slug):
     for question in question_list:
-        
-        # print("Subject Slug:", subject_slug)
-        # print("Question Number:", question.queno)
-        # print("Question:", question.question + image_to_base64(question.imgquestion) )
-        
-        # print("option1:", question.rb1 + image_to_base64(question.imgrb1))
-        # print("option2:", question.rb2 + image_to_base64(question.imgrb2))
-        # print("option3:", question.rb3 + image_to_base64(question.imgrb3))
-        # print("option3:", question.rb4 + image_to_base64(question.imgrb4))
-
-        # if(question.answer == 'a'):
-        #     print("Answer:", 0)
-        # elif(question.answer == 'b'):
-        #     print("Answer:", 1)
-        # elif(question.answer == 'c'):
-        #     print("Answer:", 2)
-        # elif(question.answer == 'd'):
-        #     print("Answer:", 3)
-        # else:
-        #     print("NoAnswer")
-
-        # print("Explanation:", question.explanation + image_to_base64(question.imgexp))
-        # print("Part:", question.part)
         print()
         question_list = {
             "chapter": subject_slug,
@@ -97,8 +74,11 @@ def prepare_data(question_list, subject_slug):
         elif question.imgexp :
             question_list["description"].append({"image": image_to_base64(question.imgexp)})
 
+
+        # print(login().text)
         print(question_list)
-        
+        print("Question Number:", question.queno)
+        # print("Question:", question.question)
         print( add_quiz(question_list).text)
         time.sleep(10)
     return 1
@@ -108,23 +88,50 @@ def prepare_data(question_list, subject_slug):
 def add_quiz(question):
     data = {'url': question}
 
+    # headers = {
+    #     # localhost
+    #     # 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJpYXQiOjE2ODkxNTE0NDcsIm5iZiI6MTY4OTE1MTQ0NywiZXhwIjoxNzUyMjIzNDQ3LCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzIn19fQ.KDap7rFKvGNMQoZKz8WfFZ6WDjsL-OwAa8ZK_x_8_Z0',
+    #     # dashencon
+    #     # 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rhc2hlbmNvbi5jb20vdGVzdCIsImlhdCI6MTY4OTE2MDYyMCwibmJmIjoxNjg5MTYwNjIwLCJleHAiOjE3NTIyMzI2MjAsImRhdGEiOnsidXNlciI6eyJpZCI6IjEyIn19fQ.m6jgrjTzXzyOV7BG1xi782RmnGMjbkMA5igY0q5ie_k',
+    #     # datascienceplc
+    #     'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RhdGFzY2llbmNlcGxjLmNvbSIsImlhdCI6MTY4OTE3MzQ5MSwibmJmIjoxNjg5MTczNDkxLCJleHAiOjE3NTIyNDU0OTEsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19',
+    # }
+
     headers = {
-        # localhost
-        # 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJpYXQiOjE2ODkxNTE0NDcsIm5iZiI6MTY4OTE1MTQ0NywiZXhwIjoxNzUyMjIzNDQ3LCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzIn19fQ.KDap7rFKvGNMQoZKz8WfFZ6WDjsL-OwAa8ZK_x_8_Z0',
-        # dashencon
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Rhc2hlbmNvbi5jb20vdGVzdCIsImlhdCI6MTY4OTE2MDYyMCwibmJmIjoxNjg5MTYwNjIwLCJleHAiOjE3NTIyMzI2MjAsImRhdGEiOnsidXNlciI6eyJpZCI6IjEyIn19fQ.m6jgrjTzXzyOV7BG1xi782RmnGMjbkMA5igY0q5ie_k',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RhdGFzY2llbmNlcGxjLmNvbSIsImlhdCI6MTY4OTE3Njc1MywibmJmIjoxNjg5MTc2NzUzLCJleHAiOjE3NTIyNDg3NTMsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.KxRGq62IH3GmafLFj-evYBCbVh1LWbKWu-E-CDZK8LA',
+        "Content-Type": "application/json",
+        "User-Agent": "PostmanRuntime/7.32.3"  # Replace with an appropriate user agent
     }
-
     # return requests.post("http://localhost:8080/api/ds_quiz/v1/contribute",
-    return requests.post("https://dashencon.com/test/api/ds_quiz/v1/contribute",
+    # return requests.post("https://dashencon.com/test/api/ds_quiz/v1/contribute",
+    return requests.post("https://datascienceplc.com/api/ds_quiz/v1/contribute",
         # proxies={'http': '222.255.169.74:8080'},
-        timeout=15, json = question, headers=headers, verify=certifi.where())
+        timeout=15, json = question, headers=headers, verify=certifi.where()
+        )
 
+########################################################################################################
+def login():
+    data = {
+                "username":"esubalew.a2009@gmail.com",
+                "password": "Herdseb@1",
+                "login_with": "email_password"
+            }
+
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "PostmanRuntime/7.32.3"  # Replace with an appropriate user agent
+    }
+    return requests.post("https://datascienceplc.com/api/auth/v1/token",
+        timeout=15, json = data, headers=headers, verify=certifi.where()
+        )
 def image_to_base64(img_data):
 
     if(isinstance(img_data, str)):
         current_dir = os.path.dirname(os.path.abspath(__file__))        
         image_path = os.path.join(current_dir, "drawable/"+img_data+".png")
+        if not os.path.exists(image_path):
+            image_path = os.path.join(current_dir, "drawable/"+img_data+".jpg")
+
         if os.path.exists(image_path):
             with open(image_path, "rb") as image_file:
                 encoded_string = base64.b64encode(image_file.read())
